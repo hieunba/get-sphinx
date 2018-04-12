4082f8900b6027ef7d223b4fa3272f05e51c06c4
Nghiem Ba Hieu <hi3unb@gmail.com>
2018-04-12 10:48:48 +0700

    Update tests to use with Python 3.x only

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -1,11 +1,12 @@
 unless os.linux?
-  describe command('python.exe --version') do
+  # Tests check with py laucher for version 3 only
+  describe command('py -3 --version') do
     its('exit_status') { should eq 0 }
-    its('stdout') { should match (/Python 3.6.5/) }
+    its('stdout') { should match (/Python 3.[5-9].[0-9]/) }
   end
-  describe command('easy_install --version') do
+  describe command('py -3 -m easy_install --version') do
     its('exit_status') { should eq 0 }
-    its('stdout') { should match (/setuptools 39.[0-9].[0-9]/) }
+    its('stdout') { should match (/setuptools ([0-9][0-9]).[0-9].[0-9]/) }
   end
   describe command('sphinx-build --version') do
     its('exit_status') { should eq 0 }
