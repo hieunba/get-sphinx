3ffe9cebd91d9e27af5b4f266666ca611e6a9859
Nghiem Ba Hieu <hi3unb@gmail.com>
2018-07-22 11:16:29 +0700

    Update Batch build for input source

--- build/docker/texlive-2017/start.ps1
+++ build/docker/texlive-2017/start.ps1
@@ -1,13 +1,48 @@
 $Docs = "C:\Docs"
 $ErrorLog = "$Docs\Errors.log"
 
-function Start-Build {
+function Get-DocumentList {
+<#
+.SYNOPSIS
+List document sources can be compiled
+.DESCRIPTION
+Find all source directories inside the given source and return a list that can be built
+.EXAMPLE
+Get-DocumentList -source C:\Docs
+.PARAMETER source
+INput source directory where to find document sources
+#>
 param(
-    [string]$source = "C:\docs"
+    [string]$source = "C:\Docs"
 )
-    cd $source
+    If (Test-Path $source) {
+         $(ls -Filter source -Attributes Directory -Recurse $source | Select FullName -ExpandProperty FullName) 
+    } Else {
+         @()
+    }
+}
 
-    If (Test-Path "$source\source") { 
+function Start-Build {
+<#
+.SYNOPSIS
+Generate HTML and PDF from sphinx source
+.DESCRIPTION
+Generate HTML and PDF from sphinx source
+.EXAMPLE
+Start-Build -source C:\Docs
+.PARAMETER source
+Document source to build
+#>
+param(
+    [string]$source = "C:\Docs"
+)
+    If (Test-Path $source\..\Makefile) {
+        cd $source\..
+    } Else {
+        cd $source
+    }
+
+    If (Test-Path $pwd\source) { 
 
         try {
            make clean html
@@ -15,29 +50,39 @@ param(
            cd build\latex
            latexmk -pdfxe
         } Catch {
-            "ERROR building for $source : $_" | Add-Content $ErrorLog
+            Write-Host -ForeGroundColor Red "ERROR building for $source : $_"
         }
     } Else {
-       "No source found. Skip building for $source" | Add-Content $ErrorLog
+       Write-Host -ForeGroundColor DarkYellow "No a valid source. Skipping building for $source"
     }
      
 }
 
-function Batch-Build {
-    If (Test-Path "$Docs\source") {
-        Write-Host "Found source directory in $Docs"
-        Start-Build -source $Docs
-    }
+function Start-BatchBuild {
+<#
+.SYNOPSIS
+Generate HTML and PDF for a list
+.DESCRIPTION
+Generate HTML and PDF for a list. This list contains document sources path.
+.EXAMPLE
+Start-BatchBuild -BatchList "C:\Docs\sphinxsource"
+.PARAMETER BatchList
+List of document source path to build. By default, it is empty.
+#>
+param(
+    [Array]$BatchList = @()
+)
+    
+    If ($BatchList.Count -eq 0) {
+        Write-Host -ForeGround DarkYellow "Document list was empty."
+        Write-Host -ForegroundColor DarkMagenta "Please checking the document source."
+    } 
     Else
     {
-       $BuildList = $(Get-ChildItem -Attributes Directory $Docs | Select Name -ExpandProperty Name)
-       If ($BuildList.Count -eq 0) {
-           tlmgr.bat --version
-       } Else {
-           Foreach ($i in $BuildList) {
-               Start-Build -source "$Docs\$i"
-           }
-       }
+        Foreach ($item in $BatchList) {
+             Write-Host -ForegroundColor Green "Building $item .." 
+             Start-Build -source "$item"
+        }
     }
 }
 
@@ -45,5 +90,5 @@ function Batch-Build {
 If ([System.IO.File]::Exists($ErrorLog)) {
     Out-Null > $ErrorLog
 }
-Batch-Build
+Start-BatchBuild -BatchList $(Get-DocumentList)
 cd $Docs
