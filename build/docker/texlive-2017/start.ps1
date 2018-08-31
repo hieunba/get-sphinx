be723d1a4f4510d94b085d34b0d65d64c120c7b9
Nghiem Ba Hieu <hi3unb@gmail.com>
2018-08-31 22:40:23 +0700

    Collect artifacts at a centralized location inside doc source

--- build/docker/texlive-2017/start.ps1
+++ build/docker/texlive-2017/start.ps1
@@ -1,6 +1,27 @@
 $Docs = "C:\Docs"
 $ErrorLog = "$Docs\Errors.log"
 
+function Create-DocumentStore {
+<#
+.SYNOPSIS
+Prepare directory structure for storing artifacts
+.DESCRIPTION
+Prepare directory structure for storing artifacts
+.EXAMPLE
+Create-DocumentStore
+.PARAMETER stores
+Input stores or a collection of directories, default, "PDF, Doc, HTML"
+#>
+param(
+    [Array]$stores = @("PDF", "HTML", "Doc")
+)
+    foreach ($item in $stores) {
+        If (-not (Test-Path "$Docs\output\$item")) {
+            mkdir "$Docs\output\$item"
+        }
+    }
+}
+
 function Get-DocumentList {
 <#
 .SYNOPSIS
@@ -16,7 +37,7 @@ param(
     [string]$source = "C:\Docs"
 )
     If (Test-Path $source) {
-         $(ls -Filter source -Attributes Directory -Recurse $source | Select FullName -ExpandProperty FullName) 
+         $(ls -Filter source -Attributes Directory -Recurse $source | Select FullName -ExpandProperty FullName)
     } Else {
          @()
     }
@@ -42,24 +63,32 @@ param(
         cd $source
     }
 
-    If (Test-Path $pwd\source) { 
+    If (Test-Path $pwd\source) {
 
+        $doc_name = $(ls | Select -ExpandProperty Parent -First 1).ToString()
         try {
            $build_dir = $pwd
            make clean singlehtml
            cd build\singlehtml
-           pandoc -o index.docx index.html
+           pandoc -o "$doc_name.docx" index.html
            cd $build_dir
            make latex
            cd build\latex
            latexmk -pdfxe
+           cd $build_dir
+           mv "build\singlehtml\$doc_name.docx" "$Docs\output\Doc"
+           If (-not (Test-Path "$Docs\output\HTML\$doc_name")) {
+               mv build\singlehtml "$Docs\output\HTML\$doc_name"
+           }
+           cp build\latex\*.pdf "$Docs\output\PDF"
+           make clean
         } Catch {
             Write-Host -ForeGroundColor Red "ERROR building for $source : $_"
         }
     } Else {
        Write-Host -ForeGroundColor DarkYellow "No a valid source. Skipping building for $source"
     }
-     
+
 }
 
 function Start-BatchBuild {
@@ -76,15 +105,16 @@ List of document source path to build. By default, it is empty.
 param(
     [Array]$BatchList = @()
 )
-    
+
     If ($BatchList.Count -eq 0) {
         Write-Host -ForeGround DarkYellow "Document list was empty."
         Write-Host -ForegroundColor DarkMagenta "Please checking the document source."
-    } 
+    }
     Else
     {
+        Create-DocumentStore
         Foreach ($item in $BatchList) {
-             Write-Host -ForegroundColor Green "Building $item .." 
+             Write-Host -ForegroundColor Green "Building $item .."
              Start-Build -source "$item"
         }
     }
